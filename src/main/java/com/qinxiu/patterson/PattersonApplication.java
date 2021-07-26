6d45d5ffe67ca9c59ac9e8162b3c5e0710883786
qwang <qwang@xmltravelgate.com>
2021-07-26 21:12:02 +0200

    -updates

--- src/main/java/com/qinxiu/patterson/PattersonApplication.java
+++ src/main/java/com/qinxiu/patterson/PattersonApplication.java
@@ -1,15 +1,24 @@
 package com.qinxiu.patterson;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.qinxiu.patterson.provider.mapper")
-public class PattersonApplication {
+public class PattersonApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(PattersonApplication.class, args);
   }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(PattersonApplication.class);
+  }
 }

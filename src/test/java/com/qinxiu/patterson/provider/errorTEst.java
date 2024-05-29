34a72b183fdbfe322d0c4b82cf9aa4c518c32ee6
qinxiu.wang <qinxiu.wang@patterson.agency>
2024-05-29 23:23:38 +0200

    updated test

---
+++ src/test/java/com/qinxiu/patterson/provider/errorTEst.java
@@ -0,0 +1,18 @@
+package com.qinxiu.patterson.provider;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * errorTEst.
+ *
+ * @Description TODO
+ * @Date 29/5/24 23:23
+ * @Created by qinxiuwang
+ */
+public class errorTEst {
+
+  @Test
+  void test(){
+    asdasdsad;
+  }
+}
